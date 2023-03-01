437605291af1eeb1afbe12e29f21a8e116272a2b
Ignacio <ignacio06cornejo@gmail.com>
2023-02-28 21:17:56 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const btn_p = document.getElementById('btn_p');
+const info = document.getElementById('info');
+
+btn_p.addEventListener('click', function() {
+ info.classList.toggle('hidden');
+});
+
+const btn_p2 = document.getElementById('btn_p2');
+const info2 = document.getElementById('info2');
+
+btn_p2.addEventListener('click', function() {
+ info2.classList.toggle('hidden');
+});
+
+const btn_p3 = document.getElementById('btn_p3');
+const info3 = document.getElementById('info3');
+
+btn_p3.addEventListener('click', function() {
+ info3.classList.toggle('hidden');
+});
+
+const btn_p4 = document.getElementById('btn_p4');
+const info4 = document.getElementById('info4');
+
+btn_p4.addEventListener('click', function() {
+ info4.classList.toggle('hidden');
+});
